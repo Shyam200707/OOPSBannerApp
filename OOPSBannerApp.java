4cb0436a80c848749430e226625f711619c98a4e
Shyam200707 <Shyamok2007@gmail.com>
2026-02-17 20:11:36 +0530

    UC2: Render OOPS banner using ASCII print statements

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,21 @@
 /**
- * OOPSBannerApp - Use Case 1
- * Prints the literal text "OOPS".
+ * OOPSBannerApp - Use Case 2
+ * Prints OOPS in Banner Format using '*' and spaces.
  *
  * @author Shyam
- * @version 1.0
+ * @version 2.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("  *****  " + "  *****  " + " ******* " + " ******* ");
+        System.out.println(" *     * " + " *     * " + " *      *" + " *       ");
+        System.out.println(" *     * " + " *     * " + " *      *" + " *       ");
+        System.out.println(" *     * " + " *     * " + " ******* " + "  *****  ");
+        System.out.println(" *     * " + " *     * " + " *       " + "       * ");
+        System.out.println(" *     * " + " *     * " + " *       " + "       * ");
+        System.out.println("  *****  " + "  *****  " + " *       " + "  *****  ");
     }
 }
+
